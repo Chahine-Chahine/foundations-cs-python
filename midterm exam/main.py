5f16c261030d9ed72a3d5cf9e60a34dc5514a5bc
Chahine-Chahine <chahinesquare@hotmail.com>
2023-07-29 02:49:52 +0300

    4th update

--- midterm exam/main.py
+++ midterm exam/main.py
@@ -1,17 +1,17 @@
 import datetime
-#datetime search from : https://www.w3schools.com/python/python_datetime.asp
+
 # Function to read tickets from the file and store them in a list of dictionaries
 def readTickets(file_path):
     tickets = []
     with open(file_path, 'r') as file:
         for line in file:
             ticket_info = line.strip().split(', ')
-          #ticket is a dictionary
+            # ticket is a dictionary
             ticket = {
                 "ticket_id": ticket_info[0],
                 "event_id": ticket_info[1],
                 "username": ticket_info[2],
-              #strptime method from https://www.geeksforgeeks.org/python-time-strptime-function/
+                # strptime method from https://www.geeksforgeeks.org/python-time-strptime-function/
                 "timestamp": datetime.datetime.strptime(ticket_info[3], '%Y%m%d'),
                 "priority": int(ticket_info[4])
             }
@@ -38,10 +38,22 @@ def addTicket(tickets, event_id, username, current_date):
     tickets.append(new_ticket)
     print("Ticket booked successfully!")
 
+def adminAddTicket(tickets, event_id, username, current_date, priority):
+    ticket_id = f"tick{str(len(tickets) + 1).zfill(3)}"
+    current_date = datetime.datetime.strptime(current_date, '%Y%m%d')
+    new_ticket = {
+        'ticket_id': ticket_id,
+        'event_id': event_id,
+        'username': username,
+        'timestamp': current_date,
+        'priority': priority
+    }
+    tickets.append(new_ticket)
+    print("Ticket booked successfully!")
 
 # Function to find the event with the highest number of tickets
 def findMostBooked(tickets):
-    events = {} #Initializing a dict
+    events = {} # Initializing a dict
     for ticket in tickets:
         event_id = ticket["event_id"]
         if event_id in events:
@@ -51,11 +63,11 @@ def findMostBooked(tickets):
 
     if not events:
         return None
-#max value in a dictionary by key https://www.youtube.com/watch?v=CJsBF2a5zsg (Indians are the best)
+    # max value in a dictionary by key https://www.youtube.com/watch?v=CJsBF2a5zsg (Indians are the best)
     most_booked_event = max(events, key=events.get)
     return most_booked_event
 
-def userMenu(tickets):  
+def userMenu(tickets):
     username = input("Username: ")
     password = input("Password: ")
 
@@ -100,20 +112,31 @@ def adminMenu(tickets):
                     if most_booked_event is not None:
                         print(f"Event ID with the highest number of tickets: {most_booked_event}")
                     else:
-                        print("No tickets available.")  
+                        print("No tickets available.")
                 elif choice == 2:
-                    pass  
+                    event_id = input("Insert event ID: ")
+                    username = input("username for the ticket booking: ")
+                    current_date = input("Input the date of the event in yyyymmdd format: ")
+                    priority = int(input("Enter priority number: "))
+                    adminAddTicket(tickets, event_id, username, current_date, priority)
+                    save = input("Do you wish to save? (write y if yes n for no)")
+                    if save == "y":
+                       saveTickets("tickets.txt", tickets)  # Save tickets before exiting
+                       print("saving...")
+                    elif save == "n":
+                      print("Not saved")
+                      
                 elif choice == 3:
-                    pass  
+                    pass
                 elif choice == 4:
-                    pass 
+                    pass
                 elif choice == 5:
-                    pass  
+                    pass
                 elif choice == 6:
-                    pass  
+                    pass
                 elif choice == 7:
                     print("Exiting Program...")
-                    return False  
+                    return False
                 else:
                     print("Invalid choice. Please try again.")
 
@@ -133,7 +156,7 @@ def main():
     if choice == "admin":
         adminMenu(tickets)
     elif choice == "user":
-        userMenu(tickets)  
+        userMenu(tickets)
     else:
         print("Invalid user type. Exiting...")
 
